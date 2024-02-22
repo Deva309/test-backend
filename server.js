161b0ece97b144a90d30c445c2e0091a82a10ef9
Dev Ashish Saradhana <devashish.3093@gmail.com>
2024-02-22 15:54:00 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = 4000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+let todos = [];
+
+app.get('/api/todos', (req, res) => {
+  res.json(todos);
+});
+
+app.post('/api/todos', (req, res) => {
+  const newTodo = req.body;
+  todos.push(newTodo);
+  res.json(newTodo);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
